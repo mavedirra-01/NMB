a95a2e89b76a95a5f249a4c3219d1ed3b14f358e
mavedirra-01 <mavedirra@mavedirra.com>
2024-06-24 19:54:40 -0300

    fixes

--- internal/cleanup/cleanup.go
+++ internal/cleanup/cleanup.go
@@ -3,6 +3,7 @@ package cleanup
 import (
 	"NMB/internal/logging"
 	"os"
+	"path/filepath"
 )
 
 func MoveCSVToProjectFolder(csvFilePath, projectFolder string) {
@@ -11,12 +12,12 @@ func MoveCSVToProjectFolder(csvFilePath, projectFolder string) {
 		return
 	}
 
-	// 	newFilePath := filepath.Join(projectFolder, filepath.Base(csvFilePath))
-	// 	err := os.Rename(csvFilePath, newFilePath)
-	// 	if err != nil {
-	// 		logging.ErrorLogger.Printf("Failed to move CSV file to project folder: %v", err)
-	// 		return
-	// 	}
+	newFilePath := filepath.Join(projectFolder, filepath.Base(csvFilePath))
+	err := os.Rename(csvFilePath, newFilePath)
+	if err != nil {
+		logging.ErrorLogger.Printf("Failed to move CSV file to project folder: %v", err)
+		return
+	}
 
-	// logging.InfoLogger.Printf("CSV file moved to project folder: %s", newFilePath)
+	logging.InfoLogger.Printf("CSV file moved to project folder: %s", newFilePath)
 }
